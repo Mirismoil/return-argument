cfe179c2b8612fe76ce2efdcad74db2f0212ae23
Mirismoil <yuldashevmirismoil3@gmail.com>
2022-08-20 03:42:29 +0500

    initial commit

---
+++ js/argumen.js
@@ -0,0 +1,8 @@
+function hayotiy(fullName, age, country, phoneNumber, work) {
+    console.log(fullName, age, country, phoneNumber, work)
+}
+hayotiy("Odil Odilov", 20, "Uzbek", 931234567, 'doctor')
+hayotiy("Samat Rajabov", 17, "Kazak", 941234567, 'webSite')
+hayotiy("Sabina Tolipova", 22, "Uzbek", 901234567, 'webDisiner')
+hayotiy("Sardor Ochilov", 18, "Uzbek", 911234567, 'smm')
+hayotiy("Toshmat Oqayev", 21, "Kazak", 971234567, 'techer')
---
+++ js/red.js
@@ -0,0 +1,14 @@
+function hayotiy( work){
+    return (work);
+}
+let yosh1=hayotiy(5);
+let nom1 = hayotiy("webSite");
+let nom2= hayotiy('doctor');
+let nom3= hayotiy( 'techer');
+let nom4= hayotiy('smm');
+let nom5= hayotiy('doctor');
+console.log(nom1)
+console.log(nom2)
+console.log(nom3)
+console.log(nom4)
+console.log(nom5)
